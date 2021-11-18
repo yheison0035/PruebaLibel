884597de7ccac0284f0b4af7712b32799159c066
yheison035 <59831171+yheison035@users.noreply.github.com>
2021-11-18 10:28:22 -0500

    Cambios

---
+++ pagina/pagprueba/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header class="header-home">
+            <nav>
+                <p>LibelStudios</p>
+                <div className="header-nav-menu">
+                    <a href="#Home">Home</a>
+                    <a href="#Home">Curso</a>
+                    <a href="#Home">Requisitos</a>
+                    <a href="#Home">Participar</a>
+                </div>
+            </nav>
+        </header>
+    )
+}
